235343f93f59c0ebc2090069780d7923d660df0e
Kyle-Ruan <kyleryk123@gmail.com>
2016-11-24 20:45:53 +1100

    more progress

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
+app.get('/users', function(req, res) {
+  res.json([
+    { id: 1, firstName: 'Bob', lastName: 'Smith', email: 'bob@gmail.com' },
+    { id: 2, firstName: 'Tammy', lastName: 'Smith', email: 'bob@gmail.com' },
+    { id: 3, firstName: 'Tina', lastName: 'Smith', email: 'bob@gmail.com' },
+    { id: 4, firstName: 'Cath', lastName: 'Smith', email: 'bob@gmail.com' },
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     //
